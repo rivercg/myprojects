2ed055c30fc8839529b7fabdf0b5058ad401641a
William <riverforeverjon@hotmail.com>
2010-10-19 15:25:31 +0800

    fix models of interview.

--- MyInterview/interviews/admin.py
+++ MyInterview/interviews/admin.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
-from models import *
+from models import Message, VirtualUser, IvRecord
 
-admin.site.register(Message)
 
 class VirtualUserAdmin(admin.ModelAdmin):
-    list_display = ('position', 'candidate', 'status', 'open_date', 'close_date')
-    list_filter = ['position', 'status', 'open_date', 'close_date']
-    search_fields = ['position', 'candidate', 'status']
-    date_hierarchy = 'open_date'
-
+    list_display = ('pk', 'user_name', 'role', 'username_pk', 'disp_name', 'email', 'last_login')
+    list_filter = ['role']
+    search_fields = ['role']
 
 class IvRecordAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -16,11 +13,18 @@ class IvRecordAdmin(admin.ModelAdmin):
         ('Status information',  {'fields': ['status']}),
         ('Date information', {'fields': ['open_date', 'close_date'], 'classes': ['collapse']}),
     ]
-    list_display = ('position', 'candidate', 'status', 'open_date', 'close_date')
+    list_display = ('pk', 'position', 'candidate', 'status', 'open_date', 'close_date')
     list_filter = ['position', 'status', 'open_date', 'close_date']
     search_fields = ['position', 'candidate', 'status']
     date_hierarchy = 'open_date'
 
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'ivrecord_pk', 'iv_record', 'ivrecord_status', 'sendfrom_pk', 'send_from', 'sendfrom_email')
+    list_filter = ['pub_date']
+    search_fields = ['text', 'iv_record', 'send_from']
+    date_hierarchy = 'pub_date'
+
 admin.site.register(VirtualUser, VirtualUserAdmin)
 admin.site.register(IvRecord, IvRecordAdmin)
+admin.site.register(Message, MessageAdmin)
 
--- MyInterview/interviews/models.py
+++ MyInterview/interviews/models.py
@@ -6,19 +6,22 @@ from candidates.models import Candidate
 MSG_MAX_LEN = 500
 
 class VirtualUser(models.Model):
-    real_user = models.ForeignKey(User)
+    real_user = models.OneToOneField(User)
     ROLE_CHOICES = (
         ('sys', 'System'),
         ('hr', 'HR'),
         ('ivr', 'Interviewer'),
     )
-    role = models.CharField(max_length=3, choices=ROLE_CHOICES)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     
     def __unicode__(self):
-        return self.real_user   
-    
+        return self.user_name() 
+
     def user_name(self):
-        return self.real_user   
+        return self.real_user.username 
+    
+    def username_pk(self):
+        return self.real_user.pk  
     
     def email(self):
         return self.real_user.email
@@ -33,7 +36,7 @@ class VirtualUser(models.Model):
         return self.real_user.last_name
     
     def disp_name(self):
-        return self.first_name() + ' ' + self.last_name()    
+        return self.first_name() + ' ' + self.last_name()
 
 class IvRecord(models.Model):
     position = models.ForeignKey(Position)
@@ -50,7 +53,8 @@ class IvRecord(models.Model):
     close_date = models.DateTimeField('date closed', blank=True, null=True)
     
     def __unicode__(self):
-        return str(self.pk)
+        return self.candidate.name() + ' => ' + self.position.title
+        #return  self.position 
     
 class Message(models.Model):
     iv_record = models.ForeignKey(IvRecord)
@@ -58,5 +62,36 @@ class Message(models.Model):
     text = models.CharField(max_length=MSG_MAX_LEN)
     pub_date = models.DateTimeField('date published')
     
-    def __unicode__(self):
+    def degist_text(self):
         return self.text
+    
+    def __unicode__(self):
+        return self.degist_text()
+    
+    def ivrecord_pk(self):
+        return self.iv_record.pk
+    
+    def ivrecord_position(self):
+        return self.iv_record.position
+    
+    def ivrecord_candidate(self):
+        return self.iv_record.candidate
+    
+    def ivrecord_status(self):
+        return self.iv_record.status
+    
+    def sendfrom_pk(self):
+        return self.send_from.pk
+    
+    def sendfrom_role(self):
+        return self.send_from.role 
+
+    def sendfrom_username(self):
+        return self.send_from.user_name()
+    
+    def sendfrom_dispname(self):
+        return self.send_from.disp_name()
+    
+    def sendfrom_email(self):
+        return self.send_from.email()
+    
